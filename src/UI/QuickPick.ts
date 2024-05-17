a0368a7924962cc1b31f0819f28ff13398986141
grzegorzkiselev <egorkakiselev@gmail.com>
2024-05-17 18:36:26 +0300

    fix: check if quickPick enabled before listen onhide

--- src/UI/QuickPick.ts
+++ src/UI/QuickPick.ts
@@ -49,7 +49,9 @@ export class SuggestionsList {
   };
 
   onHide = (callback) => {
-    this.quickPick.onDidHide(callback);
+    if (this.enabled) {
+      this.quickPick.onDidHide(callback);
+    }
   };
 
   addSuggestion = (suggestion: Suggestion) => {
